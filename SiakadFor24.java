7470d967439dbb7c1ab92c2b8d7d62de9224b18c
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-10-23 09:51:25 +0700

    Percobaan 1 (Modifikasi)

--- SiakadFor24.java
+++ SiakadFor24.java
@@ -4,10 +4,14 @@ public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int mahasiswa = 10, mahasiswaTidakLulus = 0, mahasiswaLulus;
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= mahasiswa; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            if (nilai < 60) {
+                mahasiswaTidakLulus++;
+            }
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
@@ -15,9 +19,12 @@ public class SiakadFor24 {
                 terendah = nilai;
             }
         }
+        mahasiswaLulus = mahasiswa - mahasiswaTidakLulus;
         sc.close();
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + mahasiswaLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + mahasiswaTidakLulus);
     }
 }
